a6cede3818d37c5eebd26110b3d01cfe941b93d1
Nethmini1999 <nethminidilshani99@gmail.com>
2023-05-01 00:07:54 +0530

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -3,15 +3,18 @@
     define('APPROOT', dirname(dirname(__FILE__)));
 
     //URLROOT
-    define('URLROOT', 'http://localhost/ConvoMVC');
+    define('URLROOT', 'http://localhost/Convo');
     
     //WEBSITE NAME
     define('SITENAME', 'Convo | Online Forum Website');
 
     //DATABASE Configurations
-    define('DB_HOST', 'localhost');
+    define('DB_HOST', 'localhost:3307');
     define('DB_USER', 'root');
     define('DB_PASSWORD', '');
     define('DB_NAME', 'convo');
+
+    //PUBROOT
+    define('PUBROOT', dirname(dirname(dirname(__FILE__))).'\public');
     
 ?>
