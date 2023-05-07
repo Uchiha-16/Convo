05db984ec12e233ae9da858a595600cd94c1724e
Nethmini1999 <nethminidilshani99@gmail.com>
2023-05-08 04:01:55 +0530

    Update Snavbar.php

--- app/views/inc/components/Snavbar.php
+++ app/views/inc/components/Snavbar.php
@@ -1,40 +1,47 @@
 <div class="nav">
-            <div><a href="#"><img src="<?php echo URLROOT; ?>/img/logo%20with%20name%20WHITE%201.png" id="logo"></a></div>
-            <div class="nav-hover"><a href="">Home</a></div>
-            <div class="nav-hover"><a href="">Webinar</a></div>
-            <div><input type="search" name="search" placeholder="Search for questions..."/></div>
+            
             <?php if(isset($_SESSION['userID'])) : ?>
+            <div><a href="<?php echo URLROOT; ?>/Pages/seeker"><img src="<?php echo URLROOT; ?>/img/logo%20with%20name%20WHITE%201.png" id="logo"></a></div>
+            <div class="nav-hover"><a href="<?php echo URLROOT; ?>/Pages/seeker">Home</a></div>
+            <div class="nav-hover"><a href="<?php echo URLROOT; ?>/Webinars/home">Webinar</a></div>
+            <div><input type="search" name="search" placeholder="Search for questions..."/></div>
+           
             <div class="dropdown-list">
                 <button class="dropbtn dropbtn-1" onclick="drop()"><img src="<?php echo URLROOT; ?>/img/plus.png" class="icon"> Add New</button>
                 <div class="dropdown-content" id="myDropdown">
-                    <a href="seeker/addQuestion.php">Question</a>
-                    <a href="seeker/addBlog.php" style="border-bottom:none;">Blog</a>
+                    <a href="<?php echo URLROOT;?>/Questions/add">Question</a>
+                    <a href="<?php echo URLROOT;?>/Blogs/add" style="border-bottom:none;">Blog</a>
                 </div>
             </div>
             <div class="nav-hover"><img src="<?php echo URLROOT; ?>/img/notification.png" class="nav-icon"></div>
-            <div class="nav-hover"><img src="<?php echo URLROOT; ?>/img/chat.png" class="nav-icon"></div>
-            <div class="nav-hover dropbtn" onclick="drop2()">
+            <div class="nav-hover"><a href="<?php echo URLROOT; ?>/Chats/index"><img src="<?php echo URLROOT; ?>/img/chat.png" class="nav-icon"></a></div>
+            <div class="dropbtn" onclick="drop2()">
                 <img class="dropbtn" src="<?php echo URLROOT; ?>/img/profile.png" class="nav-icon" style="width: 25px;">
                 <div class="dropdown-content dropdown-content2" id="myDropdown2">
-                    <a href="#">Profile</a>
-                    <a href="#">Approvals</a>
-                    <a href="#">Blogs</a>
-                    <a href="#">Events</a>
-                    <a href="#">Projects</a>
-                    <a href="#">Skill Test</a>
-                    <a href="seeker/subscriptionpage.php">Subscription</a>
-                    <a href="./logout.php" style="border-bottom:none">Log-out</a>
+                    <a href="<?php echo URLROOT;?>/Profiles/seeker">Profile</a>
+                    <!-- <a href="<?php // echo URLROOT;?>/Moderator/approve">Approvals</a> -->
+                    <a href="<?php echo URLROOT;?>/Blogs/index">Blogs</a>
+                    <a href="<?php echo URLROOT;?>/Events/index">Events</a>
+                    <a href="<?php echo URLROOT;?>/Projects/index">Projects</a>
+                    <a href="<?php echo URLROOT;?>/SkillTest/index">Skill Test</a>
+                    <a href="<?php echo URLROOT;?>/Subscriptions/index">Subscription</a>
+                    <a href="<?php echo URLROOT;?>/report/addReport">Report</a>
+                    <a href="<?php echo URLROOT?>/Users/logout" style="border-bottom:none">Log-out</a>
                 </div>
                 </div>
                 <?php else : ?>
+                <div><a href="<?php echo URLROOT; ?>/Pages/index"><img src="<?php echo URLROOT; ?>/img/logo%20with%20name%20WHITE%201.png" id="logo"></a></div>
+                <div class="nav-hover"><a href="<?php echo URLROOT; ?>/Pages/index">Home</a></div>
+                <div class="nav-hover"><a href="<?php echo URLROOT; ?>/Webinars/home">Webinar</a></div>
+                <div><input type="search" name="search" placeholder="Search for questions..."/></div>
                     <style>
                         .nav {
                             grid-template-columns: 5% 6% 6% 57% 6% 6% 8% 6%;
                         }
                     </style>
-                <div class="nav-hover"><a href="login.php">Login</a></div>
-                <div class="nav-hover"><a href="signup.php">Register</a></div>
-                <div class="nav-hover"><a href="aboutus.php">About us</a></div>
+                <div class="nav-hover"><a href="<?php echo URLROOT;?>/Users/login">Login</a></div>
+                <div class="nav-hover"><a href="<?php echo URLROOT;?>/Users/signup">Register</a></div>
+                <div class="nav-hover"><a href="<?php echo URLROOT; ?>/Pages/about">About us</a></div>
                 <?php endif; ?>
              
         </div>
